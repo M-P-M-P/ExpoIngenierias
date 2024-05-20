48618b3bbb1a1ff69dafdc37fe596c8b64f7cc18
CruzPer823 <a01736214@tec.mx>
2024-05-19 19:26:18 -0600

    refactor: :closed_lock_with_key: modified the env document for the database connection

--- node/app.js
+++ node/app.js
@@ -9,8 +9,6 @@ import db from "./database/db.js"
 //importamos nuestro enrutador
 //hay que importar las rutas de admin
 // las rutas de juez
-import main from './middleware/authConfig.js'
-
 
 
 
@@ -36,33 +34,6 @@ try {
 
 const sessionStore = SequelizeStore(session.Store)
 const store = new sessionStore({db:db})
-
-
-app.use(session({
-    secret: '35a73657a3sd5436873as5d74368a57sd35a7sd357asd',
-    resave: false,
-    saveUninitialized: true,
-    store: store,
-    cookie: {
-        secure: 'auto'
-    }
-}));
-
-
-
-app.get('/', (req, res)=>{
-    req.session.usuario = 'Juan Perez';
-    req.session.rol = 'Admin';
-    req.session.visitas = req.session.visitas ? ++req.session.visitas : 1;
-    res.send(`El usuario <strong>${req.session.usuario}</strong> con rol <strong>${req.session.rol}</strong> ha visitado esta pagina 
-    <strong>${req.session.visitas}</strong>
-    `)
-})
-
-
-//cron.schedule('*/20 * * * * *', function() { console.log('Running a task every 20 seconds'); main();});
-
-
 
 
 app.listen(8000, ()=>{
--- node/database/db.js
+++ node/database/db.js
@@ -4,10 +4,10 @@ import oracledb from 'oracledb';
 
 const db = new Sequelize({
     dialect: 'oracle',
-    username: 'admin',
-    password: config.DB_PASSWORD,
+    username: 'ADMIN',
+    password: 'uj@@FpVFwkZ7Uvn',
     dialectOptions: {   
-        connectString: config.DB_CONNECTION_STRING,
+        connectString: '(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.mx-queretaro-1.oraclecloud.com))(connect_data=(service_name=g10e8bcafdb4ced_nctc1obxti1z34rl_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))',
         dialectModule: oracledb
     },
     logging: console.log
