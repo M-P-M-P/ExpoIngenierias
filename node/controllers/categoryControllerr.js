532251821b3ac6e0602d79036adfc3ef8ba2e1ad
CruzPer823 <a01736214@tec.mx>
2024-05-20 16:59:45 -0600

    feat: :sparkles: Create new categories

--- node/controllers/categoryControllerr.js
+++ node/controllers/categoryControllerr.js
@@ -1,9 +1,9 @@
 import AnnounModel from "../models/AnnounceModel.js";
 
-export const createAnnounce = async(req,res)=>{
+export const createCategory = async(req,res)=>{
     try{
         const newAnnounce= await AnnounModel.create(req.body);
-        console.log("Nuevo anuncio creado: ", newAnnounce.toJSON());
+        console.log("Nuevo Categoria creada creado: ", newAnnounce.toJSON());
         res.status(201).json({newAnnounce});
     }catch (error){
         res.status(400).json({error:error.message});
---
+++ node/routes/CategoryRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express'
+export {createCategoty}
+const router = express.Router()
+
+router.post('/Create',createAnnounce);
+
+export default router;
