c355763ec38f354a7767cab017bdb285a0b06cf0
BetoxAlka <A01736390@tec.mx>
2024-05-23 21:39:23 -0600

    Merge Santos

--- node/database/db.js
+++ node/database/db.js
@@ -1,16 +1,19 @@
-import { Sequelize } from 'sequelize';
-import config from '../config.js';
+import config from '../config.js'
+import { Sequelize } from "sequelize";
+import oracledb from 'oracledb';
 
-const sequelize = new Sequelize({
-  dialect: 'oracle',
-  username: 'admin',
-  password: config.DB_PASSWORD,
-  database: config.DB_NAME,
-  host: config.DB_HOST,
-  port: config.DB_PORT,
-  dialectOptions: {
-    connectString: config.DB_CONNECTION_STRING,
-  },
+const db = new Sequelize({
+    dialect: 'oracle',
+    username: 'admin',
+    password: config.DB_PASSWORD,
+    dialectOptions: {   
+        connectString: config.DB_CONNECTION_STRING,
+        dialectModule: oracledb
+    },
+    logging: console.log
 });
 
-export default sequelize;
+
+
+
+export default db
