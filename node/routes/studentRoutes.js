064668253a6a22ede9f9c6338db6a01bc1a0e914
BetoxAlka <A01736390@tec.mx>
2024-05-23 21:40:43 -0600

    Merge Santos

--- node/routes/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { fetchAllStudents , findStudentById} from '../controllers/studentController.js';
-
-const router = express.Router();
-
-router.get('/students', fetchAllStudents);
-router.get('/students/:id', findStudentById);
-
-export default router;
