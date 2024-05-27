ee976a030553551dccbbe7ca38ab1736fb1e4dc3
HectoR3yes <77632055+HectoR3yes@users.noreply.github.com>
2024-05-27 04:46:58 -0600

    complete manual judge assignation integration
    (stable)

--- src/Components/Table/JudgeRow.js
+++ src/Components/Table/JudgeRow.js
@@ -5,18 +5,21 @@ import { useParams } from 'react-router-dom';
 function JudgeRow({ user }) {
     const { projectId } = useParams(); // Use react-router-dom to get the projectId from the URL
     const [assigned, setAssigned] = useState(false);
-    
+    const [alert, setAlert] = useState({ type: '', message: '', visible: false });
+
     const assignJudge = () => {
         axios.post(`http://localhost:8000/Admin/assignProjectJudge`, {
-            judgeId: user.id, // Assuming judgeId is fixed for now
-            projectId: projectId // Assuming projectId is taken from the URL
+            judgeId: user.id, // Using the user.id for the judgeId
+            projectId: projectId // Using the projectId from the URL
         })
         .then(response => {
             // Handle successful assignment
             setAssigned(true);
+            setAlert({ type: 'success', message: 'Juez asignado de forma exitosa!', visible: true });
         })
         .catch(error => {
-            console.error('There was an error assigning the judge to the project:', error);
+            console.error('El juez no pudo ser asignado', error);
+            setAlert({ type: 'danger', message: 'Error assigning judge. Please try again.', visible: true });
         });
     };
 
@@ -25,9 +28,11 @@ function JudgeRow({ user }) {
         .then(response => {
             // Handle successful removal
             setAssigned(false);
+            setAlert({ type: 'success', message: 'Juez eliminado de forma exitosa!', visible: true });
         })
         .catch(error => {
-            console.error('There was an error removing the judge from the project:', error);
+            console.error('There was an error removing the judge', error);
+            setAlert({ type: 'danger', message: 'El juez no puede ser eliminado', visible: true });
         });
     };
 
@@ -50,21 +55,29 @@ function JudgeRow({ user }) {
     }, [projectId, user.id]);
 
     return (
-        <tr key={user.id}>
-            {/* First column */}
-            <td className="text-center">
-                {user.name}
-            </td>
-        
-            {/* Second column - Conditionally render button based on isJudge */}
-            <td className="text-center">
-                {assigned ? (
-                    <button className="btn btn-danger" onClick={removeJudge}>Eliminar</button>
-                ) : (
-                    <button className="btn btn-primary custom-primaty-btn" onClick={assignJudge}>Asignar</button>
-                )}
-            </td>
-        </tr>
+        <>
+            {alert.visible && (
+                <div className={`alert alert-${alert.type} alert-dismissible fade show`} role="alert">
+                    {alert.message}
+                    <button type="button" className="btn-close" onClick={() => setAlert({ ...alert, visible: false })}></button>
+                </div>
+            )}
+            <tr key={user.id}>
+                {/* First column */}
+                <td className="text-center">
+                    {user.name}
+                </td>
+            
+                {/* Second column - Conditionally render button based on assigned */}
+                <td className="text-center">
+                    {assigned ? (
+                        <button className="btn btn-danger" onClick={removeJudge}>Eliminar</button>
+                    ) : (
+                        <button className="btn btn-primary custom-primary-btn" onClick={assignJudge}>Asignar</button>
+                    )}
+                </td>
+            </tr>
+        </>
     );
 }
 
